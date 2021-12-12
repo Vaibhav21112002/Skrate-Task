05016303521d2a50891dac00fa3534a194da9d04
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-12-13 01:07:17 +0530

    Initialized Files and Folders

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import School from "./Pages/School/School";
+import Buisness from "./Pages/Buisness/Buisness";
+import Work from "./Pages/Work/Work";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/school" element={<School />} />
+        <Route path="/buisness" element={<Buisness />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Sidebar = () => {
+  return <div>This is Sidebar</div>;
+};
+
+export default Sidebar;
